e1653c592b26104d7a79b12925c63130702ea89f
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-04-25 10:40:46 +0200

    correcting errors

--- AI/index.js
+++ AI/index.js
@@ -1,62 +1,60 @@
-import express from "express";
-import cors from "cors";
-import { getAIResponse } from "./communicationManager.js";
+// Author: Arnau Fernandez Gil
+// Institut Pedralbes 2024 2DAW
 
+// Importing required modules
+import express from "express"; // Importing Express framework
+import cors from "cors"; // Importing CORS middleware
+import { getAIResponse } from "./communicationManager.js"; // Importing the function that communicates with the AI
 
-const app = express()
+// Initializing Express application
+const app = express();
+
+// Applying CORS middleware to allow cross-origin requests
 app.use(cors());
-app.use(express.json())
-const port = 3000
 
-app.post('/test', async (req, res) => {
-  console.log("test");
-});
+// Parsing incoming requests with JSON payloads
+app.use(express.json());
 
+// Defining the port number for the server to listen on
+const port = 3000;
+
+// Handling POST requests to the root endpoint ('/')
 app.post('/', async (req, res) => {
-
-  console.log(req.body.content);
-
+  
+  // Setting the response header with content type and encoding information
   res.writeHead(200, {
     "Content-type": "text/plain",
     "transfer-encoding": "chunked"
-  })
+  });
 
+  // Calling an asynchronous function to get a response from an AI system, passing the content from the request body
+  const response = await getAIResponse(req.body.content);
 
-  const response = await getAIResponse(req.body.content)
-
+  // Iterating over the response body in chunks
   for await (const chunk of response.body) {
-
-    // console.log("sending response");
-
     
+    // Converting each chunk of data from buffer to a UTF-8 encoded string
+    const jsonString = Buffer.from(chunk).toString('utf8');
 
-    // console.log("Sent: ", chunk);
-
-  
-    const jsonString = Buffer.from(chunk).toString('utf8')
-
+    // Checking if the length of the string exceeds a certain threshold
     if (jsonString.length > 270) {
+      
+      // Slicing the string to remove certain characters (assuming it's some kind of header or metadata)
+      const reducedString = jsonString.slice(6);
 
-      const reducedString = jsonString.slice(6)
-
-      // const parsedData = JSON.parse(reducedString)
-
-      res.write(reducedString)
-
-      // message += parsedData.choices[0].delta.content
-
-      // console.log(message);
+      // Writing the reduced string to the response
+      res.write(reducedString);
     }
   }
 
+  // Logging a message indicating that the processing is done
   console.log("done");
 
-  res.end()
-  
-})
+  // Ending the response
+  res.end();
+});
 
-
-
+// Starting the server and listening on the defined port
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
