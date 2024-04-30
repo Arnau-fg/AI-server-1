bebb625a339309e4cf2416430ae42373d889d730
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-04-30 09:11:05 +0200

    Posada ruta https

--- frontAI/communicationManager.js
+++ frontAI/communicationManager.js
@@ -1,6 +1,6 @@
 export async function fetchNode() {
     //Change the host to your Node's host
-    return (await fetch("http://192.168.218.52:3000", {
+    return (await fetch("https://ia.inspedralbes.cat", {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
