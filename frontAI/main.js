b0b56c7d33be0092ee27fe3ecee47b790dbb97c3
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-05-22 09:08:18 +0200

    trets comentaris

--- frontAI/main.js
+++ frontAI/main.js
@@ -114,22 +114,22 @@ let utf8decoder = new TextDecoder();
 // Fetching a stream of data asynchronously using the 'iaCasualitza' function
 // const stream = await iaCasualitza(objectToPrompt, categories);
 
-// // Iterating over the stream of data asynchronously, processing each chunk, stream.body is required, because the chunk data is stored in the body attribute
-// for await (const chunk of stream.body) {
-//   // Decoding the chunk of data from UTF-8 encoding to a string
-//   const stringData = utf8decoder.decode(chunk);
+// Iterating over the stream of data asynchronously, processing each chunk, stream.body is required, because the chunk data is stored in the body attribute
+for await (const chunk of stream.body) {
+  // Decoding the chunk of data from UTF-8 encoding to a string
+  const stringData = utf8decoder.decode(chunk);
 
-//   // Parsing the decoded string data to an object
-//   const parsedData = JSON.parse(stringData);
+  // Parsing the decoded string data to an object
+  const parsedData = JSON.parse(stringData);
 
-//   // Extracting the message from the parsed data and appending it to the 'message' variable
-//   message += parsedData.choices[0].delta.content;
+  // Extracting the message from the parsed data and appending it to the 'message' variable
+  message += parsedData.choices[0].delta.content;
 
-//   showMessage = message.replace("upgrade", 'millorar');
+  showMessage = message.replace("upgrade", 'millorar');
 
-//   // Setting the inner text of the DOM element 'textDOM' to the accumulated message
-//   textDOM.innerText = showMessage;
-// }
+  // Setting the inner text of the DOM element 'textDOM' to the accumulated message
+  textDOM.innerText = showMessage;
+}
 
 function createArray(content, element) {
   let objectToPush;
