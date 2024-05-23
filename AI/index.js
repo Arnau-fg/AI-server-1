da3b44ac9a53bc2323f8e2377f0e249c4d513a0d
Arnau-fg <a22arnfergil@inspedralbes.cat>
2024-05-23 10:34:05 +0200

    millores allargar

--- AI/index.js
+++ AI/index.js
@@ -36,6 +36,8 @@ app.post('/', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -52,11 +54,18 @@ app.post('/', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -78,6 +87,8 @@ app.post('/info', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -94,11 +105,18 @@ app.post('/info', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -119,6 +137,8 @@ app.post('/genera', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -135,11 +155,18 @@ app.post('/genera', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -160,6 +187,8 @@ app.post('/millora', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -167,6 +196,7 @@ app.post('/millora', async (req, res) => {
     const jsonString = Buffer.from(chunk).toString('utf8');
 
     tokenCounter++;
+
 
     // Checking if the length of the string exceeds a certain threshold, this is becuse the last info sent throws an error when trying to parse it
     if (jsonString.length > 270) {
@@ -176,11 +206,18 @@ app.post('/millora', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -188,7 +225,7 @@ app.post('/millora', async (req, res) => {
 
 app.post('/allarga', async (req, res) => {
   // The system prompt tells the AI how to act
-  const systemContent = "You are a professional recruiter and a digital portfolio expert, you work for an app called KnowMe, this app is the best portfilio creation app. You help people by aiding their requests to make a digital portfolio. The user will send you an object, some data and some categories. Have in mind the categories when creating your answer. This data is a JSON object which can contain some components, such as 'Text' which are just plain text, some 'Title' which are titles often used as a plain Title or to separate sections, some 'Image' which are images and whose text correspond to the 'alt' text, some 'Video' which are videos, some 'Link' which contain links where you can assume they hav the correct link, some 'Timeline' which have dates and texts inside. This data is what they already have in their portfolio. This data is ordered by how far up in the portfolio it is. Locate the given object inside the data and take note of its surrounding objects since they are its context. Rewrite the content inside the object and make it so that its length is 125% of the original. YOU ARE NOT THE PERSON YOU ARE TRYING TO HELP. Give ONLY the text, nothing else. Answer in Catalan. Don't add the english translation at the end";
+  const systemContent = "You are a professional recruiter and a digital portfolio expert, you work for an app called KnowMe, this app is the best portfilio creation app. You help people by aiding their requests to make a digital portfolio. The user will send you an object and some categories. Rewrite the content inside the object and make it so that its length is 125% of the original, don't make it a lot longer. YOU ARE NOT THE PERSON YOU ARE TRYING TO HELP. Give ONLY the text, nothing else. Answer in Catalan. Don't add the english translation at the end. Answer only the content, nothing else. Don't use these character: \".";
   // Counter o know how many tokens have been sent
   let tokenCounter = 0;
 
@@ -201,6 +238,8 @@ app.post('/allarga', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -208,6 +247,7 @@ app.post('/allarga', async (req, res) => {
     const jsonString = Buffer.from(chunk).toString('utf8');
 
     tokenCounter++;
+
 
     // Checking if the length of the string exceeds a certain threshold, this is becuse the last info sent throws an error when trying to parse it
     if (jsonString.length > 270) {
@@ -217,11 +257,18 @@ app.post('/allarga', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -242,6 +289,8 @@ app.post('/acurta', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
@@ -249,6 +298,7 @@ app.post('/acurta', async (req, res) => {
     const jsonString = Buffer.from(chunk).toString('utf8');
 
     tokenCounter++;
+
 
     // Checking if the length of the string exceeds a certain threshold, this is becuse the last info sent throws an error when trying to parse it
     if (jsonString.length > 270) {
@@ -258,11 +308,18 @@ app.post('/acurta', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -283,12 +340,16 @@ app.post('/formalitza', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
     // Converting each chunk of data from buffer to a string
     const jsonString = Buffer.from(chunk).toString('utf8');
+
     tokenCounter++;
+
 
     // Checking if the length of the string exceeds a certain threshold, this is becuse the last info sent throws an error when trying to parse it
     if (jsonString.length > 270) {
@@ -298,11 +359,18 @@ app.post('/formalitza', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
@@ -323,12 +391,17 @@ app.post('/casualitza', async (req, res) => {
   // Calling an asynchronous function to get a response from the AI, passing the content from the request body
   const response = await getCreativeAIResponse(systemContent, req.body.content);
 
+  let finalMessage = "";
+
+
   // Iterating over the response body in chunks, stream.body is required, because the chunk data is stored in the body attribute
   for await (const chunk of response.body) {
 
     // Converting each chunk of data from buffer to a string
     const jsonString = Buffer.from(chunk).toString('utf8');
+
     tokenCounter++;
+
 
     // Checking if the length of the string exceeds a certain threshold, this is becuse the last info sent throws an error when trying to parse it
     if (jsonString.length > 270) {
@@ -338,11 +411,18 @@ app.post('/casualitza', async (req, res) => {
 
       // Writing the reduced string to the response
       res.write(reducedString);
+
+      let msg = JSON.parse(reducedString);
+
+      finalMessage += msg.choices[0].delta.content;
+
     }
   }
 
   // Logging a message indicating that the processing is done
   console.log("done, tokens: ", tokenCounter);
+
+  console.log("final message: ", finalMessage);
 
   // Ending the response
   res.end();
